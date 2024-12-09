ff804ee743b0ac42e200457032a2ecd7d5ad2017
Xoffers <167771147+Xoffers@users.noreply.github.com>
2024-12-09 15:36:08 +0200

    Add files via upload

--- LAB10/functions.js
+++ LAB10/functions.js
@@ -33,3 +33,24 @@ window.onscroll = function() {
 scrollToTopBtn.onclick = function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+document.getElementById('saveButton').addEventListener('click', function() {
+    const form = document.getElementById('contact-forms');
+    const formData = new FormData(form);
+
+    let dataObject = {};
+    formData.forEach((value, key) => {
+        dataObject[key] = value;
+    });
+    
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML='';
+
+    for (const [key, value] of Object.entries(dataObject)) {
+        const p = document.createElement('p');
+        p.textContent = `${key}: ${value}`;
+    }
+
+    console.log(dataObject);
+    document.getElementById('result').innerHTML = JSON.stringify(dataObject, null, 2)
+});
